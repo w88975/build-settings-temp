5ac2cfa1af633b892f3ca47f075124e4a6dd513b
kamisama <1457434343@qq.com>
2015-03-11 16:18:59 +0800

    build settings 增加配置文件 增加输入检查等

--- editor/elements/build-settings.js
+++ editor/elements/build-settings.js
@@ -1,6 +1,9 @@
 var Remote = require('remote');
 var dialog = Remote.require('dialog');
 var Url = require('fire-url');
+var Fs = require('fire-fs');
+var projectPath = Remote.getGlobal('FIRE_PROJECT_PATH');
+var Path = require('fire-path');
 
 Polymer({
     publish: {
@@ -17,34 +20,164 @@ Polymer({
         defaultPlatform: "Mobile",
         isDebug: true,
         defaultScene: "",
-        defaultDistPath: "",
+        defaultBuildPath: "",
         projectName: "",
+        buildSceneList: [], //scene list for buid
     },
 
     created: function () {
         this.ipc = new Fire.IpcListener();
-        this.allScene = [];
+
+        this.settingPath = Path.join( projectPath, 'settings' ) + "/build-settings.json";
+        this.isInvalidName = false;
+        this.isInvalidPath = false;
+        var loadFile = false;
+        
+        this.loadConfig(function (data,err,errMsg) {
+            if (!err) {
+                this.isDebug = data.isDebug;
+                this.defaultScene = data.defaultScene;
+                this.defaultBuildPath = data.BuildPath;
+                this.defaultPlatform = data.platform;
+                this.defaultPlatformConfig = data.platformConfig;
+                this.projectName = data.projectName;
+                this.sceneList = data.sceneList;
+                loadFile = true;
+            }
+            else {
+                loadFile = false;
+            }
+
+        }.bind(this));
+
+        if (!loadFile) {
+            this.projectName = Path.basename(projectPath);
+            this.defaultBuildPath = projectPath;
+        }
     },
 
     attached: function () {
         Fire.sendToCore('asset-db:build-settings:getLibrarylist');
         this.ipc.on('asset-db:build-settings:SceneList', function ( results ) {
-            this.allScene = [];
+            this.sceneList = [];
             for ( var i = 0; i < results.length; ++i ) {
                 var item = results[i];
-                this.allScene.push( { name: item.url, value: item.uuid } );
+                this.sceneList.push( { name: item.url, value: item.uuid, noignore: true, } );
             }
-            this.sceneList = this.allScene;
             this.defaultScene = this.sceneList[0].value;
         }.bind(this) );
-
     },
 
     chooseDistPath: function () {
-        dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections' ]},function (res) {
+        dialog.showOpenDialog({ defaultPath: projectPath, properties: ['openDirectory', 'multiSelections' ]},function (res) {
             if (res) {
-                this.defaultDistPath = res;
+                this.defaultBuildPath = res;
             }
         }.bind(this));
+    },
+
+    selectChanged: function (event) {
+        for (var i = 0; i < this.sceneList.length; ++i) {
+            if (this.sceneList[i].value === this.defaultScene) {
+                this.sceneList[i].noignore = true;
+            }
+        }
+    },
+
+    getBuildList: function () {
+        var buildList = [];
+        for (var i = 0; i < this.sceneList.length; ++i) {
+            if (this.sceneList[i].noignore === true) {
+                buildList.push(this.sceneList[i].value);
+            }
+        }
+        this.buildSceneList = buildList;
+    },
+
+    saveConfig: function () {
+        var settings = {
+            projectName: this.projectName,
+            defaultScene: this.defaultScene,
+            platform: this.defaultPlatform,
+            platformConfig: this.defaultPlatformConfig,
+            BuildPath: this.defaultBuildPath,
+            isDebug: this.isDebug,
+            buildSceneList: this.buildSceneList,
+            sceneList: this.sceneList,
+        };
+
+        var settingsJson = JSON.stringify(settings, null, 2);
+        Fs.writeFile(this.settingPath, settingsJson, 'utf8', function ( err ) {
+            if ( err ) {
+                Fire.error( err.message );
+                return;
+            }
+            Fire.log('Start Building...');
+        }.bind(this));
+    },
+
+    loadConfig: function (callback) {
+        var exists = Fs.existsSync(this.settingPath);
+        if (!exists)
+            return;
+        Fs.readFile(this.settingPath, 'utf8', function ( err, data ) {
+            try {
+                data = JSON.parse(data);
+            }
+            catch (e) {
+                Fire.error(e);
+                callback(data,true,e);
+                return;
+            }
+
+            callback(data,false);
+        });
+    },
+
+    pathInputBlurAction: function () {
+        if ( !Fs.existsSync(this.defaultBuildPath) || !Fs.statSync(this.defaultBuildPath).isDirectory() ) {
+            Fire.warn('build dir not eixsts');
+            this.$.path.setAttribute("invalid","");
+            this.isInvalidPath = true;
+            return;
+        }
+        this.$.path.removeAttribute("invalid");
+        this.isInvalidPath = false;
+        if (!this.isInvalidName && !this.isInvalidPath) {
+            this.$.tip.style.display = "none";
+        }
+    },
+
+    projectNameBlurAction: function () {
+        if ( this.$.projName.value === "" ) {
+            Fire.warn('build dir not eixsts');
+            this.$.projName.setAttribute("invalid","");
+            this.isInvalidName = true;
+            return;
+        }
+        this.$.projName.removeAttribute("invalid");
+        this.isInvalidName = false;
+        if (!this.isInvalidName && !this.isInvalidPath) {
+            this.$.tip.style.display = "none";
+        }
+    },
+
+    BuildAction: function () {
+        if (!this.isInvalidName && !this.isInvalidPath) {
+            this.$.tip.style.display = "none";
+            this.getBuildList();
+            this.saveConfig();
+            // TODO:
+        }else {
+            this.$.tip.style.display = "block";
+            this.$.tip.animate([
+                { color: "white" },
+                { color: "red" },
+                { color: "white" },
+                { color: "red" },
+            ], {
+                duration: 300
+            });
+        }
     },
 });
